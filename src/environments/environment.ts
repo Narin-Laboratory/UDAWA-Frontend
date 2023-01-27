54758e0246e2837dd9083e4fb1dae9e4598d0b63
iwas108 <64204327+iwas108@users.noreply.github.com>
2023-01-27 13:17:05 +0800

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCy4ElNv1XkANNNRwKH7hV85ZlnHok0gvE",
+        authDomain: "udawa108.firebaseapp.com",
+        projectId: "udawa108",
+        storageBucket: "udawa108.appspot.com",
+        messagingSenderId: "660795907920",
+        appId: "1:660795907920:web:635299f4efb0652ab59655",
+        measurementId: "G-KD0X452832"
+    }
+  };
